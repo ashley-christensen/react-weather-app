92390eee55f6d39d1a21444684e5ad2e26861dac
Ashley Christensen <ashleyccode@yahoo.com>
2022-04-20 08:42:08 -0700

    final functionality, dynamic icons and weather info

--- src/components/SearchMain.js
+++ src/components/SearchMain.js
@@ -1,10 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import WeatherDetails from './WeatherDetails';
 
 const SearchMain = () => {
   const [searchTerm, setSearchTerm] = useState('oakland');
-  console.log(searchTerm);
+  const [tempInfo, setTempInfo] = useState({});
 
+  const getWeatherInfo = async () => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=imperial&appid=08204476b31df54b176fb679d7a8ee23`;
+      const response = await fetch(url);
+      const data = await response.json();
+      const { temp, humidity, pressure } = data.main;
+      const { main: weatherType } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const newWeatherInfo = {
+        name,
+        temp,
+        humidity,
+        pressure,
+        weatherType,
+        speed,
+        country,
+        sunset,
+      };
+      setTempInfo(newWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
   return (
     <>
       <div className='wrap'>
@@ -16,10 +46,12 @@ const SearchMain = () => {
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
+          <button className='searchButton' onClick={getWeatherInfo}>
+            Search
+          </button>
         </div>
-        <button className='searchButton'>Search</button>
       </div>
-      <WeatherDetails />
+      <WeatherDetails {...tempInfo} />
     </>
   );
 };
--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -1,19 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const WeatherDetails = () => {
+const WeatherDetails = ({
+  name,
+  temp,
+  humidity,
+  pressure,
+  weatherType,
+  speed,
+  country,
+  sunset,
+}) => {
+  const [weatherState, setWeatherState] = useState('');
+
+  useEffect(() => {
+    if (weatherType) {
+      switch (weatherType) {
+        case 'Clouds':
+          setWeatherState('wi-day-cloudy');
+          break;
+        case 'Haze':
+          setWeatherState('wi-fog');
+          break;
+        case 'Snow':
+          setWeatherState('wi-day-snow-wind');
+          break;
+        case 'Clear':
+          setWeatherState('wi-day-sunny');
+          break;
+        case 'Mist':
+          setWeatherState('wi-dust');
+          break;
+        case 'Rain':
+          setWeatherState('wi-day-rain');
+          break;
+        default:
+          setWeatherState('wi-day-sunny');
+          break;
+      }
+    }
+  }, [weatherType]);
+
+  //converting the seconds in time
+  const sec = sunset;
+  const date = new Date(sec * 1000);
+  let timeStr = `${date.getHours()}:${date.getMinutes()}`;
+
   return (
     <>
       <article className='widget'>
         <div className='weatherIcon'>
-          <i className={`wi wi-sunset`}></i>
+          <i className={`wi ${weatherState}`}></i>
         </div>
         <div className='weatherInfo'>
           <div className='temperature'>
-            <span>29&deg;</span>
+            <span>{temp}&deg;</span>
           </div>
           <div className='description'>
-            <div className='weatherCondition'>Sunny</div>
-            <div className='place'>Oakland, USA</div>
+            <div className='weatherCondition'>{weatherType}</div>
+            <div className='place'>
+              {name}, {country}
+            </div>
           </div>
         </div>
         <div className='date'>{new Date().toLocaleString()}</div>
@@ -24,7 +70,7 @@ const WeatherDetails = () => {
                 <i className={'wi wi-sunset'}></i>
               </p>
               <p className='extra-info-leftside'>
-                6:57 PM <br />
+                {timeStr}PM <br />
                 Sunset
               </p>
             </div>
@@ -34,7 +80,7 @@ const WeatherDetails = () => {
                 <i className={'wi wi-humidity'}></i>
               </p>
               <p className='extra-info-leftside'>
-                333 <br />
+                {humidity} <br />
                 Humidity
               </p>
             </div>
@@ -46,7 +92,7 @@ const WeatherDetails = () => {
                 <i className={'wi wi-rain'}></i>
               </p>
               <p className='extra-info-leftside'>
-                333 <br />
+                {pressure} <br />
                 Pressure
               </p>
             </div>
@@ -56,7 +102,7 @@ const WeatherDetails = () => {
                 <i className={'wi wi-strong-wind'}></i>
               </p>
               <p className='extra-info-leftside'>
-                422 <br />
+                {speed} <br />
                 Speed
               </p>
             </div>
